337f98ff2100bd021c1e3c516041c3b33af20288
Diego Lagos <diego@myorb.com>
2020-04-07 17:55:44 +0100

    updated start file

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ docker run --restart=always --network=orthanc --name orthanc -itd -p 4242:4242 -
 docker run --restart=always --network=orthanc --name meddream -itd -p 8081:80 -e integration=token \
   -v $PWD/config/application.properties:/opt/meddream/application.properties:ro \
   -v $PWD/config/system.json:/opt/meddream/sys/settings/system.json:ro \
-  -v $PWD/config/meddream.lic:/opt/meddream/license/meddream.lic \
+  -v $PWD/config/meddream.lic:/opt/meddream/license/meddream.lic:ro \
   meddream/orthanc-dicom-viewer
 docker run --restart=always --network=orthanc --name meddream-token-service -itd -p 8088 meddream/token-service
 docker run --restart=always --network=orthanc -d -p 8080:80 --name his meddream/dicom-viewer-integration
