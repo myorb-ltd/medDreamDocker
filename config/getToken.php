4bc523b3a53a1380aecba78ea10a6809c4618153
Diego Lagos <diego@myorb.com>
2020-04-08 14:19:15 +0100

    change the getToken.php in his to use as a local service

---
+++ config/getToken.php
@@ -0,0 +1,47 @@
+<?php
+
+if (!isset($_GET["study"])) exit;
+// Example: http://192.168.1.121:8080/getToken.php?study=1.113654.3.13.1026
+
+// Example: curl -X POST -H "Content-Type: application/json" -d @./test.json http://172.18.0.2:8088/v1/generate
+$url = 'http://meddream-token-service:8088/v1/generate';
+
+function getToken($url, $studyUID)
+{
+  // validating studyUID
+  if ((strlen($studyUID) > 64) || (!preg_match("/^[0-9.]+$/", $studyUID)))
+    return "";
+
+  //Initiate cURL.
+  $ch = curl_init($url);
+
+  $jsonDataEncoded='{
+   "items": [{
+     "studies": {
+       "study": "'.$studyUID.'",
+       "storage": "Orthanc"
+     }
+   }]
+  }';
+
+  //print_r($jsonDataEncoded);
+
+  //Tell cURL that we want to send a POST request.
+  curl_setopt($ch, CURLOPT_POST, 1);
+
+  //Attach our encoded JSON string to the POST fields.
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+
+  //Set the content type to application/json
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+  //Execute the request
+  return curl_exec($ch);
+}
+
+  $studyUID = $_GET["study"];
+  $result = getToken($url, $studyUID);
+  $result = substr($result, 0, strlen($result)-1);
+  echo $result;
+?>
+
--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ docker run --restart=always --network=orthanc --name meddream -itd -p 8081:80 -e
   -v $PWD/config/meddream.lic:/opt/meddream/license/meddream.lic:ro \
   meddream/orthanc-dicom-viewer
 docker run --restart=always --network=orthanc --name meddream-token-service -itd -p 8088 meddream/token-service
-docker run --restart=always --network=orthanc -d -p 8080:80 --name his meddream/dicom-viewer-integration
+docker run --restart=always --network=orthanc -d -p 8080:80 --name his \
+  -v $PWD/config/getToken.php:/var/www/html/getToken.php:ro \
+  meddream/dicom-viewer-integration
 docker run --rm --network=orthanc dcm4che/dcm4che-tools:5.16.3 storescu -cDCM4CHEE@orthanc:4242 /opt/dcm4che/etc/testdata/dicom
 
